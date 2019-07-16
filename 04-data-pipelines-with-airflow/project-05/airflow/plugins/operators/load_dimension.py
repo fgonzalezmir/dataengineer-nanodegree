e18c456f823643df27f65f6180bcbaffbe9ab5f5
Federico Gonzalez <fgonzalezmir@gmail.com>
2019-07-17 00:07:21 +0200

    feat: Project 5 Data Pipelines with Airflow

---
+++ 04-data-pipelines-with-airflow/project-05/airflow/plugins/operators/load_dimension.py
@@ -0,0 +1,29 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 sql_stmt="",
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_stmt = sql_stmt
+
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info(f"Loading dimension table {self.table}")
+        sql = """TRUNCATE TABLE {}; 
+                    INSERT INTO {}
+                    {}; 
+                    COMMIT;""".format(self.table, self.table, self.sql_stmt)
+        redshift.run(sql)
