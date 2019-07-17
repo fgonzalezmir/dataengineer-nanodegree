21b9bd7978d344fd53e848ea86f43cb6f7b7a133
Federico Gonzalez <fgonzalezmir@gmail.com>
2019-07-17 20:13:16 +0200

    feat: Project 5 Data Pipelines with Airflow attmept 2, add append parameter to load dimensions operator

--- 04-data-pipelines-with-airflow/project-05/airflow/plugins/operators/load_dimension.py
+++ 04-data-pipelines-with-airflow/project-05/airflow/plugins/operators/load_dimension.py
@@ -2,6 +2,7 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
@@ -11,6 +12,7 @@ class LoadDimensionOperator(BaseOperator):
                  redshift_conn_id="",
                  table="",
                  sql_stmt="",
+                 append=False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
@@ -18,12 +20,25 @@ class LoadDimensionOperator(BaseOperator):
         self.redshift_conn_id = redshift_conn_id
         self.table = table
         self.sql_stmt = sql_stmt
+        self.append = append
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         self.log.info(f"Loading dimension table {self.table}")
-        sql = """TRUNCATE TABLE {}; 
+        sql = ""
+
+        if self.append:
+            sql = """
+                    BEGIN;
+                    INSERT INTO {}
+                    {};
+                    COMMIT;""".format(self.table, self.sql_stmt)
+        else:
+            sql = """
+                    BEGIN;
+                    TRUNCATE TABLE {}; 
                     INSERT INTO {}
                     {}; 
                     COMMIT;""".format(self.table, self.table, self.sql_stmt)
+
         redshift.run(sql)
